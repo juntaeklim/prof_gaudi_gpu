71945f14ccc860aca87727b46b4ceda6042c4447
juntaeklim <juntaek0425@gmail.com>
2024-10-02 09:38:33 +0900

    a100

---
+++ gpu_gemm/gemm.py
@@ -0,0 +1,150 @@
+import torch
+import argparse
+import time
+import vtrain_profiler as vp
+from custom_gather import gather_v_i32_indices
+from custom_scatter import scatter_v_i32_indices
+
+def run():
+    parser = argparse.ArgumentParser(description="Gather GPU test")
+ 
+    parser.add_argument("--M", type=int, default=128)
+    parser.add_argument("--K", type=int, default=128)
+    parser.add_argument("--N", type=int, default=128)
+    parser.add_argument("--gpu-n", type=int, default=0)
+    parser.add_argument("--test", action="store_true", default=False)
+    parser.add_argument("--method", type=str, choices=["time", "vtrain", "nsys", "ncomp"])
+    parser.add_argument("--dtype", type=str, choices=["bf16", "fp16", "fp32"])
+    args = parser.parse_args()
+    
+    M = args.M
+    K = args.K
+    N = args.N
+    gpu_n = args.gpu_n
+    test = args.test
+    method = args.method
+    if args.dtype == "bf16":
+        dtype = torch.bfloat16
+    elif args.dtype == "fp16":
+        dtype = torch.float16
+    elif args.dtype == "fp32":
+        dtype = torch.float32
+    else:
+        assert False
+        
+    device = torch.device("cuda:%d" %gpu_n)
+    
+    if test:
+        n_warmup = 1
+        n_iter = 0
+
+        M = 4
+        K = 4
+        N = 4        
+    else:
+        # Profiler #############################
+        if method == "vtrain" or method == "time":
+            n_warmup = 15
+            n_iter = 5
+            
+            if method == "vtrain":
+                vp.init_trace()
+            else:
+                result = []
+        elif method == "nsys" or method =="ncomp":
+            n_warmup = 2
+            n_iter = 0
+            
+            if method == "nsys":
+                torch.cuda.cudart().cudaProfilerStart()
+        else:
+            assert False
+        ########################################
+
+    
+    for i in range(n_warmup + n_iter):
+        # Tensor preparation ###################
+        input_0_tensor_cpu = torch.arange(M * K, dtype=dtype).view(M, K)
+        input_1_tensor_cpu = torch.arange(K * N, dtype=dtype).view(K, N)
+        
+        input_0_tensor = input_0_tensor_cpu.to(device)
+        input_1_tensor = input_1_tensor_cpu.to(device)
+        ########################################
+        
+        # Profiler #############################
+        if not test and method == "time" and i >= n_warmup:
+            torch.cuda.synchronize(device=device)
+            start = time.time()
+        ########################################
+        
+        ## Kernel execution ####################
+        output_tensor = torch.mm(input_0_tensor, input_1_tensor)
+        ########################################
+        
+        # Profiler #############################
+        if not test and method == "time" and i >= n_warmup:
+            torch.cuda.synchronize(device=device)
+            end = time.time()
+            result.append((end - start) * (10**6)) # sec -> usec
+        ########################################
+        
+        if test:
+            print()
+            print("input_0_tensor")
+            print(input_0_tensor)
+            print(input_0_tensor.dtype)
+            print()
+            print("input_1_tensor")
+            print(input_1_tensor)
+            print(input_1_tensor.dtype)
+            print()
+            
+            print("output_tensor")
+            print(output_tensor)
+            print(output_tensor.dtype)
+            
+    # Profiler #############################
+    if not test and method == "time":
+        assert len(result) == n_iter
+        print("result")
+        print(result)
+        result_tensor = torch.tensor(result)
+        
+        print("M, K, N, time (us)")
+        final_time = result_tensor.mean()
+        print("%d, %d, %d, %f" %(M, K, N, final_time))
+    elif not test and method == "vtrain":
+        trace = vp.finish_trace().strip().split('\n')
+        start_times = []
+        durations = []
+        for i in range(len(trace)):
+            row = trace[i]
+            splitted_row = row.split(",")
+            if splitted_row[2] == "KERNEL":
+                start_times.append(float(splitted_row[0]) / 1000)
+                durations.append(float(splitted_row[1]) / 1000)
+        
+        assert len(start_times) == n_warmup + n_iter
+        assert len(durations) == n_warmup + n_iter
+        
+        result_0 = []
+        
+        for i in range(n_warmup + n_iter):
+            result_0.append(durations[i])
+        
+        print("result_0")
+        print(result_0)
+
+        result_0_tensor = torch.tensor(result_0)
+        
+        final_time_0 = result_0_tensor[-n_iter:].mean()
+
+        print()
+        print("M, K, N, kernel_duration (us)")
+        print("%d, %d, %d, %f" %(M, K, N, final_time_0)) 
+    elif not test and method == "nsys":
+        torch.cuda.cudart().cudaProfilerStop()
+    ########################################
+    
+if __name__ == "__main__":
+    run()
---
+++ gpu_gemm/run_gemm.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Input Arguments
+type=$1      # linear or power
+start=$2     # Starting value
+end=$3       # Ending value
+stride=$4    # Stride value
+fixed_size=$5
+method=$6
+bench=$7
+dim_size=$8
+
+# Check if required arguments are provided
+if [[ -z "$type" || -z "$start" || -z "$end" || -z "$stride" || -z "$fixed_size" || -z "$method" || -z "$bench" || -z "$dim_size" ]]; then
+    echo "Missing shell script arguments"
+    exit 1
+fi
+
+# Generate values based on type (linear or power)
+if [ "$type" == "linear" ]; then
+    variables=()
+    for ((i=start; i<=end; i+=stride)); do
+        variables+=($i)
+    done
+elif [ "$type" == "power" ]; then
+    variables=()
+    value=$start
+    while [ $value -le $end ]; do
+        variables+=($value)
+        value=$((value * stride))
+    done
+else
+    echo "Invalid type. Please choose 'linear' or 'power'."
+    exit 1
+fi
+
+echo "Fixed size: $fixed_size"
+echo "Variables ${variables[@]}"
+
+# Run the loop for M, K, N values
+for variable in "${variables[@]}"
+do
+    if [[ "$bench" == "gather_v" || "$bench" == "gather_s" || "$bench" == "gups_gather" || "$bench" == "gups_update" ]]; then
+        file_name="${bench}_${method}_input_${fixed_size}_output_${variable}_dim_${dim_size}.txt"
+    elif [[ "$bench" == "scatter_v" || "$bench" == "scatter_s" ]]; then
+        file_name="${bench}_${method}_input_${variable}_output_${fixed_size}_dim_${dim_size}.txt"
+    fi
+
+	output_file="./logs/${file_name}"
+    echo $output_file
+	if [ -f "$output_file" ]; then
+        echo "File $output_file already exists. Skipping..."
+        continue
+    fi
+
+    if [[ "$bench" == "gups_update" || "$bench" == "gups_gather" || "$bench" == "gather_s" || "$bench" == "gather_v" ]]; then
+        echo "input_size: $fixed_size, output_size: $variable"
+        python gather_scatter.py --input-size ${fixed_size} --output-size ${variable} --dim-size ${dim_size} --method ${method} --bench ${bench} --using-prepared-indices --algo randint --custom > ${output_file}
+    elif [[ "$bench" == "scatter_s" || "$bench" == "scatter_v" ]]; then
+        echo "input_size: $variable, output_size: $fixed_size"
+        python gather_scatter.py --input-size ${variable} --output-size ${fixed_size} --dim-size ${dim_size} --method ${method} --bench ${bench} --using-prepared-indices --algo randint --custom > ${output_file}
+    fi
+done
